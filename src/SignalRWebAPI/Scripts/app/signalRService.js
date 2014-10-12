ab692367ee106e1778947d1a4187b5df3af10807
Matthew Nichols <matthew@matthew-nichols.com>
2014-10-12 13:34:06 -0600

    Wired up all the connection events to Console

--- src/SignalRWebAPI/Scripts/app/signalRService.js
+++ src/SignalRWebAPI/Scripts/app/signalRService.js
@@ -6,6 +6,33 @@
         //Set up SignalR 
         channelHub = $.connection.channelHub;
 
+        //#region Wire up all the connection events to the Console.
+        $.connection.hub.connectionSlow(function(e) {
+            console.log("connectionSlow", e);
+        });
+
+        $.connection.hub.disconnected(function (e) {
+            console.log("disconnect", e);
+        });
+        
+        $.connection.hub.error(function (e) {
+            console.log("error", e);
+        });
+
+        $.connection.hub.received(function (e) {
+            console.log("received", e);
+        });
+
+        $.connection.hub.reconnected(function (e) {
+            console.log("reconnect", e);
+        });
+
+        $.connection.hub.stateChanged(function (e) {
+            console.log("stateChanged", e);
+        });
+
+        //#endregion
+
         //Called by server-side hub when a registered channel is called on the WebApi
         channelHub.client.receiveMessage = function(message) {
             utilities.log("recieveMessage " + message);
